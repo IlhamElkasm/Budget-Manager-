1a1b170472f2d6c3614e90e19f2970ba2a0b1ad1
ily-123 <elkasmiilham994@gmail.com>
2024-05-16 17:18:07 +0100

    file jsp

--- Budget-Managermvc/src/main/java/Managermvc/controller/maincontroller.java
+++ Budget-Managermvc/src/main/java/Managermvc/controller/maincontroller.java
@@ -1,8 +1,14 @@
 package Managermvc.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transaction;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class maincontroller {
@@ -19,4 +25,13 @@ public class maincontroller {
 		m.addAttribute("title", "add transaction");
 		 return "Transaction";
 	 }
+	
+	//handle add transaction
+	@RequestMapping(value = "/handle-transaction", method = RequestMethod.POST)
+	public RedirectView handleTransaction(@ModelAttribute Transaction trans, HttpServletRequest request) {
+		System.out.println(trans);
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(request.getContextPath() + "/");
+		return redirectView;
+	}
 }
